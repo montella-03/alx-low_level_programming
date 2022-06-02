1d57a306ba78987d664a0a9eb84cd7682c607de3
CHRISTOPHER OIGO <83244477+montella-03@users.noreply.github.com>
2022-06-02 22:28:39 +0300

    QUOTE

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,11 +6,11 @@
 *
 *Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
 *
-* Return: always 0
+* Return: Always 0 (Success)
 */
 
 int main(void)
 {
-          write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
-          return (1);
+        write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+        return (1);
 }
