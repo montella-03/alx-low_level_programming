013f34166ffcb8a3e3a172171a583128096975a2
CHRISTOPHER OIGO <83244477+montella-03@users.noreply.github.com>
2022-06-02 22:25:23 +0300

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,15 @@
 #include <unistd.h>
 
 /**
-* main - Prints string to standard error
-* Return: 1
-**/
+* main - Entry point
+*
+*Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+*
+* Return: always 0
+*/
 
 int main(void)
 {
-write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
-return (1);
+          write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+          return (1);
 }
