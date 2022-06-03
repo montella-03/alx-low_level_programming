2845aa6efc566ab914fccfde7eac7c1ba8ba2860
montella-03 <oigoc60@gmail.com>
2022-06-03 15:04:31 +0300

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,12 +11,12 @@ int main(void)
 
 	for (i1 = 48; i1 <= 57; i1++)
 	{
-		for (i2 = i1+1; i2 <= 57; i2++)
+		for (i2 = i1 + 1; i2 <= 57; i2++)
 		{
 			putchar(i1);
 			putchar(i2);
 
-			if ((i1 != 56 && i2 != 57))
+			if (!(i1 == 56 && i2 == 57))
 			{
 				putchar(',');
 				putchar(' ');
@@ -24,6 +24,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
