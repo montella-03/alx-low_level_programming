792348523fe947d79403f544572777c6ff729a35
montella-03 <oigoc60@gmail.com>
2022-06-03 12:41:37 +0300

    nventing is a combination of brains and materials. The more brains you use, the less material you need

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+/*
+*main- Entry point
+*
+*Description- code that prints all possible different combinations of two digits
+*
+*Return 0- Always (success)
+*
+*/
+
+int main (void)
+{
+    int i1, i2;
+
+	for (i1 = 48; i1 <= 57; i1++)
+	{
+		for (i2 = 48; i2 <= 57; i2++)
+		{
+			putchar(i1);
+			putchar(i2);
+
+			if (!(i1 == 57 && i2 == 57))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+    return 0;
+}
