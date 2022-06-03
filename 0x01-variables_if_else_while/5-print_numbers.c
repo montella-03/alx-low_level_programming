6d99d4cba8f2b0af4f0494409fbceb1f88ff711c
montella-03 <oigoc60@gmail.com>
2022-06-03 12:10:41 +0300

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/*
+*main- Entry point
+*
+*Description- code that print single digit numbers
+*
+*Return 0- Always (success)
+*
+*/
+
+int main ()
+{
+    int i;
+    for(i=0; i<10; i++)
+    {
+        printf("%d\n", i);
+    }
+    return 0;
+}
