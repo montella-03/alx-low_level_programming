c22f58c0c8f2d221cfea95a8ce4a0f379f01ab04
montella-03 <oigoc60@gmail.com>
2022-06-03 12:22:45 +0300

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/*
+*main- Entry point
+*
+*Description- code that prints alphabet in reverse
+*
+*Return 0- Always (success)
+*
+*/
+
+int main ()
+{
+    char alpha= 122;
+
+	while (alpha >= 97)
+	{
+		putchar(alpha);
+		alpha--;
+	}
+
+	putchar('\n');
+
+    return 0;
+}
