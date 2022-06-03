79dc8fa0d1ce3c69aebe73fe4ee9e917955c873a
montella-03 <oigoc60@gmail.com>
2022-06-03 12:57:20 +0300

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+/*
+*main- Entry point
+*
+*Description- code that prints all possible different combinations of two digits 0-99
+*
+*Return 0- Always (success)
+*
+*/
+
+int main (void)
+{
+    
+	int i1, i2, i3, i4;
+
+	for (i1 = 48; i1 <= 57; i1++)
+	{
+		for (i2 = 48; i2 <= 57; i2++)
+		{
+			for (i3 = i1; i3 <= 57; i3++)
+			{
+				if (i3 == i1)
+				{
+					i4 = i2 + 1;
+				}
+				else
+				{
+					i4 = 48;
+				}
+
+				for (i4 = i4; i4 <= 57; i4++)
+				{
+					putchar(i1);
+					putchar(i2);
+					putchar(' ');
+					putchar(i3);
+					putchar(i4);
+
+					if (!(i1 == 57 && i2 == 56
+					      && i3 == 57 && i4 == 57))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
