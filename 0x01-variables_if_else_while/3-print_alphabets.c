c7aa922a3a3f73568f228a147674f12c16024704
montella-03 <oigoc60@gmail.com>
2022-06-03 11:46:35 +0300

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/*
+*main- Entry point
+*
+*Description- code that print alphabet in lowercase and uppercase
+*
+*Return 0- Always (success)
+*
+*/
+
+int main (void)
+{
+    char alpha;
+    for(alpha=97; alpha< 122; alpha++)
+    {
+        putchar(alpha);
+    }
+    for(alpha=65; alpha< 90; alpha++)
+    {
+        putchar(alpha);
+    }
+    putchar('\n');
+    return 0;
+}
