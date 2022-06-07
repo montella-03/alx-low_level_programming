7c5973a1bac1fd24279bf3cd57b719a0b96516b5
montella-03 <oigoc60@gmail.com>
2022-06-07 11:06:12 +0300

    here are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "alx.h"
+
+/**
+* print - a function that prints the last digit of a number.
+* @i: int to be evaluated
+* Return: last digit 
+**/
+
+int print_last_digit(int i)
+{
+	int result;
+
+	result = i % 10;
+
+	if (result < 0)
+	{
+		result *= -1;
+	}
+	_putchar('0' + result);
+	return (result);
+}
