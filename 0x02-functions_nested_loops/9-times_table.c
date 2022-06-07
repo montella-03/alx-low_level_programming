51be562f12f6705e3bdbc1b20a4af37e0e021648
montella-03 <oigoc60@gmail.com>
2022-06-07 11:20:33 +0300

    9. Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include "alx.h"
+
+/**
+* times_table - function that prints the 9 times table, starting with 0.
+* Return: nothing
+**/
+
+void times_table(void)
+{
+	int number, count, num1, num2, product;
+
+	count = 0;
+
+	while (count <= 9)
+	{
+		number = 0;
+		while (number <= 9)
+		{
+			product = count * number;
+
+			if (product < 10)
+			{
+				if (number > 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(product + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				num2 = product % 10;
+				num1 = product / 10;
+				_putchar(num1 + '0');
+				_putchar(num2 + '0');
+			}
+			number++;
+		}
+		_putchar('\n');
+		count++;
+	}
+}
