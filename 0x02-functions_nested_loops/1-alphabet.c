146ca6b5d10e77aa8a777e804c1bfd16b85bcfbd
montella-03 <oigoc60@gmail.com>
2022-06-07 10:17:47 +0300

    play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "alx.h"
+
+/**
+ * main- prints alphapets in lowercase
+ * Return- nothing
+ */
+
+void print_alphabet(void);
+{
+	char alphabet = 'a';
+	while(alphabet <= 'z')
+	{
+		_putchar(alphabet);
+		alphabet ++;
+	}
+	_putchar('\n');
+}
