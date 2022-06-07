02a65f621d0547e3b8cc82d0e35e532e4f870432
montella-03 <oigoc60@gmail.com>
2022-06-07 10:46:24 +0300

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "header.h"
+
+/**
+* _isalpha -  Checks for alphabet characters
+* @c: int to be checked
+* Return: 1, on success
+**/
+
+int _isalpha(int c);
+{
+	if (c >= 65 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
