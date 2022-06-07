6a293e645212527fd920cc0b270eafb65c4c95c7
montella-03 <oigoc60@gmail.com>
2022-06-07 10:41:45 +0300

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "header.h"
+
+/**
+* _islower -  Checks for lowercase characters
+* @c: int to be checked
+* Return: 1, on success
+**/
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
