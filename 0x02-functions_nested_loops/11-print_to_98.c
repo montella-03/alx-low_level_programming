7fbd38957e682ab13be85e72317ebd536f024a97
montella-03 <oigoc60@gmail.com>
2022-06-07 11:32:42 +0300

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "alx.h"
+
+/**
+* print - prints all natural numbers from n to a given number
+* @i: number to start count from
+* Return: nothing
+**/
+
+void print_to_98(int i)
+{
+	if (i <= 98)
+	{
+		while (i < 98)
+		{
+			printf("%d, ", i);
+			i++;
+		}
+	}
+	else
+		while (i > 98)
+		{
+			printf("%d, ", i);
+			i--;
+		}
+	printf("%d\n", i);
+}
