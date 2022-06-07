cb8b2d63f5fb696bb228fa5d843cc5f576a111dd
montella-03 <oigoc60@gmail.com>
2022-06-07 11:24:51 +0300

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "alx.h"
+
+/**
+* prints - a function that adds two numbers.
+* @i: int to add
+* @j: int to add
+* Return: an int
+**/
+
+int add(int i, int j)
+{
+	int result;
+
+	result = i + j;
+	return (result);
+}
