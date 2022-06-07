1cc9ad39b40fa3c215483863ac1e4e89d1b40b91
montella-03 <oigoc60@gmail.com>
2022-06-07 10:37:26 +0300

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "header.h"
+
+/**
+* print- Prints lowercase alphabets 10 times
+* Return: nothing
+**/
+
+void print_alphabet_x10(void)
+{
+	int i, alphabet;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		{
+			_putchar(alphabet);
+		}
+		_putchar('\n');
+	}
+}
