d70335495f96df51a9c3030c9810511e2c58c06f
montella-03 <oigoc60@gmail.com>
2022-06-09 12:13:46 +0300

    triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+* print_triangle - draws a triangle in the terminal.
+* @size: size of triangle
+* Return: nothing
+**/
+
+void print_triangle(int size)
+{
+	if (size > 0)
+	{
+		int count, space, hashs;
+
+		count = 0;
+
+		while (count < size)
+		{
+			space = count;
+
+			while (space < size - 1)
+			{
+				_putchar(' ');
+				space++;
+			}
+			hashs = 0;
+
+			while (hashs <= count)
+			{
+				_putchar('#');
+				hashs++;
+			}
+			_putchar('\n');
+			count++;
+		}
+	}
+	else
+		_putchar('\n');
+}
