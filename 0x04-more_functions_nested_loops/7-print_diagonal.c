08fa68b967e737c5f128abdf650c8dc99643ab41
montella-03 <oigoc60@gmail.com>
2022-06-09 11:51:54 +0300

    I feel like I am diagonally parked in a parallel universe

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* print_diagonal - draws a diagonal line in the terminal.
+* @n: length of line
+* Return: nothing
+**/
+
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		int count, space_count;
+
+		count = 0;
+
+		while (count < n)
+		{
+			space_count = 0;
+			while (space_count < count)
+			{
+				_putchar(' ');
+				space_count++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			count++;
+		}
+	}
+	else
+		_putchar('\n');
+}
