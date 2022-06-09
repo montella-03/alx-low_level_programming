9122855c010ee683c78509d030eb9480671fb809
montella-03 <oigoc60@gmail.com>
2022-06-09 12:09:16 +0300

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+* main - prints the numbers, from 1 to 100
+* Return: 0
+**/
+
+int main(void)
+{
+	int num1;
+
+	num1 = 1;
+
+	for (num1 = 1; num1 <= 100; num1++)
+	{
+		if (num1 % 3 == 0 || num1 % 5 == 0 || num1 % 15 == 0)
+		{
+			if (num1 % 3 == 0 && num1 % 15 != 0)
+			{
+				printf("Fizz");
+			}
+			else if (num1 % 5 == 0 && num1 % 15 != 0)
+			{
+				printf("Buzz");
+			}
+			else if (num1 % 15 == 0)
+			{
+				printf("FizzBuzz");
+			}
+		}
+		else
+			printf("%d", num1);
+
+		if (num1 < 100)
+		{
+			printf(" ");
+		}
+	}
+		printf("\n");
+		return (0);
+}
