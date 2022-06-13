270349557d225c225daccafacc05153c9ad79d26
montella-03 <oigoc60@gmail.com>
2022-06-13 11:36:32 +0300

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,14 +8,14 @@
 
 int _strlen(char *s)
 {
-    int length, i = 0;
+int length, i = 0;
 
-    while (s[i] != '\0')
-    {
-        i++;
-    }
-    length = i;
-    return (length);
+while (s[i] != '\0')
+{
+i++;
+}
+length = i;
+return (length);
 }
 
 /**
@@ -28,14 +28,14 @@ int _strlen(char *s)
 
 char *_strcpy(char *dest, char *src)
 {
-    int i;
-    char *strP;
-    int length = _strlen(src);
+int i;
+char *strP;
+int length = _strlen(src);
 
-    for (i = 0; i < length; i++)
-    {
-        dest[i] = src[i];
-    }
-    strP = dest;
-    return (strP);
+for (i = 0; i < length; i++)
+{
+dest[i] = src[i];
+}
+strP = dest;
+return (strP);
 }
