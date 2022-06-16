0ef57a926ed9a84206fb8241ab19610904de3f76
montella-03 <oigoc60@gmail.com>
2022-06-16 11:13:13 +0300

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,16 +9,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-    int i;
+int i;
 
-    i = 0;
-    while (s1[i] != '\0')
-    {
-        if (s1[i] > s2[i])
-            return (s1[i] - s2[i]);
-        if (s1[i] < s2[i])
-            return (s1[i] - s2[i]);
-        i++;
-    }
-    return (0);
+i = 0;
+while (s1[i] != '\0')
+{
+if (s1[i] > s2[i])
+return (s1[i] - s2[i]);
+if (s1[i] < s2[i])
+return (s1[i] - s2[i]);
+i++;
+}
+return (0);
 }
