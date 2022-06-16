302ae4b39e763a8a85af07cca82387e2793dc31b
montella-03 <oigoc60@gmail.com>
2022-06-16 11:12:59 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,24 +8,24 @@
 
 char *leet(char *a)
 {
-    int i, j;
+int i, j;
 
-    char s[] = "a4e3o0t7l1";
+char s[] = "a4e3o0t7l1";
 
-    i = 0;
-    j = 0;
+i = 0;
+j = 0;
 
-    while (a[i] != '\0')
-    {
-        for (j = 0; j < 10; j += 2)
-        {
-            if (a[i] == s[j] || a[i] == s[j] - 32)
-            {
-                a[i] = s[j + 1];
-                break;
-            }
-        }
-        i++;
-    }
+while (a[i] != '\0')
+{
+for (j = 0; j < 10; j += 2)
+{
+if (a[i] == s[j] || a[i] == s[j] - 32)
+{
+a[i] = s[j + 1];
+break;
+}
+}
+i++;
+}
     return (a);
 }
