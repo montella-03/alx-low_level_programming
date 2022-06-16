3c801f2b6515aebf19ca0f8df29a956a7d3ff769
montella-03 <oigoc60@gmail.com>
2022-06-16 11:13:18 +0300

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,28 +10,28 @@
 
 void print_buffer(char *b, int size)
 {
-    int c, d;
+int c, d;
 
-    c = 0;
-    if (size <= 0)
-    {
-        printf("\n");
-    }
+c = 0;
+if (size <= 0)
+{
+printf("\n");
+}
 
-    while (c < size)
-    {
-        printf("%8.8x:", c);
-        d = 0;
-        while (d < 10)
-        {
-            printf("%02x", b[c + d]);
-            if ((d % 2 == 0 && d != 0) || (c + d > size - 1))
-            {
-                printf(" ");
-            }
-            d++;
-        }
-        c += 10;
-        printf("\n");
-    }
+while (c < size)
+{
+printf("%8.8x:", c);
+d = 0;
+while (d < 10)
+{
+printf("%02x", b[c + d]);
+if ((d % 2 == 0 && d != 0) || (c + d > size - 1))
+{
+    printf(" ");
+}
+d++;
+}
+c += 10;
+printf("\n");
+}
 }
