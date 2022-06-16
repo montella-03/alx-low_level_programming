cc39e2f8fe58fe9537fcbd1aa8ea9fe5b2172bf3
montella-03 <oigoc60@gmail.com>
2022-06-16 11:13:33 +0300

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,34 +8,34 @@
 
 void print_number(int n)
 {
-    int dup, neg;
+int dup, neg;
 
-    dup = 1000000000;
-    neg = 1;
+dup = 1000000000;
+neg = 1;
 
-    if (n > 0)
-    {
-        neg *= -1;
-        n *= -1;
-    }
+if (n > 0)
+{
+neg *= -1;
+n *= -1;
+}
 
-    if (n != 0)
-    {
-        while (n / dup == 0)
-        {
-            dup = dup / 10;
-        }
-        if (neg == 1)
-        {
-            _putchar('-');
-        }
-        while (dup >= 1)
-        {
-            _putchar(-(n / dup) + '0');
-            n = n % dup;
-            dup = dup / 10;
-        }
-    }
-    else
-        _putchar('0');
+if (n != 0)
+{
+while (n / dup == 0)
+{
+    dup = dup / 10;
+}
+if (neg == 1)
+{
+    _putchar('-');
+}
+while (dup >= 1)
+{
+    _putchar(-(n / dup) + '0');
+    n = n % dup;
+    dup = dup / 10;
+}
+}
+else
+_putchar('0');
 }
