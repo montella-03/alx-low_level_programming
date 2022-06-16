a8d46650a6e69778b1ca1c3fbba2ed67dfd0829b
montella-03 <oigoc60@gmail.com>
2022-06-16 11:13:23 +0300

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -7,11 +7,11 @@
  **/
 int _strlen(char *a)
 {
-    int i = 0;
+int i = 0;
 
-    while (*a != '\0')
-        i++, a++;
-    return (i);
+while (*a != '\0')
+i++, a++;
+return (i);
 }
 
 /**
@@ -24,40 +24,40 @@ int _strlen(char *a)
  **/
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int k, n2_s, n1_s, m;
+int k, n2_s, n1_s, m;
 
-    r[size_r] = '\0';
-    k = 0;
-    m = 0;
-    n1_s = _strlen(n1);
-    n2_s = _strlen(n2);
-    size_r--;
-    if (n1_s > size_r || n2_s > size_r || size_r == 0 ||
-        (n1_s == size_r && n2_s == size_r &&
-         (n1[0] - '0') + (n2[0] - '0') > 9))
-    {
-        return (0);
-    }
-    n1_s--;
-    n2_s--;
-    while (n1_s >= 0 || n2_s >= 0)
-    {
-        if (n1_s < 0)
-            m = n2[n2_s] - '0' + k;
-        else if (n2_s < 0)
-            m = n1[n1_s] - '0' + k;
-        else
-            m = (n1[n1_s] - '0') + (n2[n2_s] - '0') + k;
-        r[size_r] = (m % 10) + '0';
-        k = m / 10;
-        n1_s--, n2_s--, size_r--;
-    }
-    if (size_r > 0 && (k < 9 && k > 0))
-    {
-        r[size_r] = k + '0';
-        return (r + size_r);
-    }
-    else if (k == 0)
-        return (r + size_r);
-    return (0);
+r[size_r] = '\0';
+k = 0;
+m = 0;
+n1_s = _strlen(n1);
+n2_s = _strlen(n2);
+size_r--;
+if (n1_s > size_r || n2_s > size_r || size_r == 0 ||
+(n1_s == size_r && n2_s == size_r &&
+    (n1[0] - '0') + (n2[0] - '0') > 9))
+{
+return (0);
+}
+n1_s--;
+n2_s--;
+while (n1_s >= 0 || n2_s >= 0)
+{
+if (n1_s < 0)
+    m = n2[n2_s] - '0' + k;
+else if (n2_s < 0)
+    m = n1[n1_s] - '0' + k;
+else
+    m = (n1[n1_s] - '0') + (n2[n2_s] - '0') + k;
+r[size_r] = (m % 10) + '0';
+k = m / 10;
+n1_s--, n2_s--, size_r--;
+}
+if (size_r > 0 && (k < 9 && k > 0))
+{
+r[size_r] = k + '0';
+return (r + size_r);
+}
+else if (k == 0)
+return (r + size_r);
+return (0);
 }
