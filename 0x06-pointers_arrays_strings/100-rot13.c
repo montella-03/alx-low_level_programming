fc93d286637b044da9cd1489f01fab9ff0c9e017
montella-03 <oigoc60@gmail.com>
2022-06-16 11:13:38 +0300

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,24 +8,24 @@
 
 char *rot13(char *a)
 {
-    int i = 0;
+int i = 0;
 
-    while (a[i] != '\0')
+while (a[i] != '\0')
+{
+if ((a[i] >= 'a' && a[i] <= 'm') ||
+(a[i] >= 'A' && a[i] <= 'M'))
     {
-        if ((a[i] >= 'a' && a[i] <= 'm') ||
-            (a[i] >= 'A' && a[i] <= 'M'))
-        {
-            a[i] = a[i] + 13;
-        }
-        else
-        {
-            while ((a[i] >= 'n' && a[i] <= 'z') ||
-                   (a[i] >= 'N' && a[i] <= 'Z'))
-            {
-                a[i] = a[i] - 13;
-            }
-        }
-        i++;
-    }
-    return (a);
+a[i] = a[i] + 13;
+}
+else
+{
+while ((a[i] >= 'n' && a[i] <= 'z') ||
+(a[i] >= 'N' && a[i] <= 'Z'))
+{
+a[i] = a[i] - 13;
+}
+}
+i++;
+}
+return (a);
 }
